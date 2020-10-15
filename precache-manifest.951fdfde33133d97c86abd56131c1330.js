df2f6bebfce880f8d161d26f36cf54598957f969
Ivan La Rocca <ivanlarocca12@gmail.com>
2020-10-15 10:36:12 -0400

    Updates

---
+++ precache-manifest.951fdfde33133d97c86abd56131c1330.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6861124632e7e125ae594379c5d3c4",
+    "url": "/laptop_customizer/index.html"
+  },
+  {
+    "revision": "e715377d7c61614c4a82",
+    "url": "/laptop_customizer/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "0801e985a08028591842",
+    "url": "/laptop_customizer/static/js/2.ed960d5b.chunk.js"
+  },
+  {
+    "revision": "e715377d7c61614c4a82",
+    "url": "/laptop_customizer/static/js/main.66a4915d.chunk.js"
+  },
+  {
+    "revision": "54eb8c3b7e85a3776709",
+    "url": "/laptop_customizer/static/js/runtime~main.11bd55f9.js"
+  }
+]);
